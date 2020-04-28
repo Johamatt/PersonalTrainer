e8bc737bec3041d8e2bc1368d2847489d695caa6
Johannes <johamatt94@gmail.com>
2020-04-29 00:50:29 +0300

    First Submit

--- src/components/Customerlist.js
+++ src/components/Customerlist.js
@@ -7,16 +7,17 @@ import Addtraining from './Addtraining';
 import { Table, Button, Input, Select, } from 'antd';
 import 'antd/dist/antd.css';
 import 'antd/dist/antd.css';
+import ReactDOM from 'react-dom';
 
 
-import Highlighter from 'react-highlighter'
+import Highlighter from 'react-highlight-words'
 import { SearchOutlined } from '@ant-design/icons';
 
 
 
 
 
-export default function Customerlist()  {
+export default function Customerlist() {
   const [customers, setCustomers] = useState([]);
   const [open, setOpen] = useState(false);
   const [msg, setMsg] = useState('');
@@ -30,68 +31,68 @@ export default function Customerlist()  {
 
   const getCustomers = () => {
     fetch('https://customerrest.herokuapp.com/api/customers')
-    .then(response => response.json())
-    .then(data => setCustomers(data.content))
-    .catch(err => console.error(err))
+      .then(response => response.json())
+      .then(data => setCustomers(data.content))
+      .catch(err => console.error(err))
   }
 
   const deleteCustomer = (customer) => {
     console.log(customer)
 
     if (window.confirm('Are you sure?')) {
-      fetch(customer, 
+      fetch(customer,
         {
           method: 'DELETE',
           headers: {
-            'Content-Type':'application/json'
-          },    
-          body: JSON.stringify(customer)  
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(customer)
         }
       )
-      .then(_ => getCustomers())
-      .then(_ => {
-        setMsg('Customer deleted');
-        setOpen(true);
-      })
-      .catch(err => console.error(err))
+        .then(_ => getCustomers())
+        .then(_ => {
+          setMsg('Customer deleted');
+          setOpen(true);
+        })
+        .catch(err => console.error(err))
     }
   }
 
-  
+
 
   const addCustomer = (customer) => {
     fetch('https://customerrest.herokuapp.com/api/customers',
       {
         method: 'POST',
         headers: {
-          'Content-Type':'application/json'
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify(customer)
       }
-    )  
-    .then(_ => getCustomers())
-    .then(_ => {
-      setMsg('New customer added');
-      setOpen(true);
-    })
-    .catch(err => console.error(err))  
+    )
+      .then(_ => getCustomers())
+      .then(_ => {
+        setMsg('New customer added');
+        setOpen(true);
+      })
+      .catch(err => console.error(err))
   }
 
-  const updateCustomer = (link,customer) => {
+  const updateCustomer = (link, customer) => {
     fetch(link, {
       method: 'PUT',
       headers: {
-        'Content-Type':'application/json'
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(customer)
     }
     )
-    .then(_ => getCustomers())
-    .then(_ => {
-      setMsg('Customer updated');
-      setOpen(true);
-    })
-    .catch(err => console.error(err));
+      .then(_ => getCustomers())
+      .then(_ => {
+        setMsg('Customer updated');
+        setOpen(true);
+      })
+      .catch(err => console.error(err));
   }
 
   const handleClose = () => {
@@ -102,19 +103,19 @@ export default function Customerlist()  {
       title: 'First Name',
       dataIndex: 'firstname',
       key: 'firstname',
-      sorter: (a, b) => { return a.firstname.localeCompare(b.firstname)},
+      sorter: (a, b) => { return a.firstname.localeCompare(b.firstname) },
     },
     {
       title: 'Last Name',
       dataIndex: 'lastname',
       key: 'lastname',
-      sorter: (a, b) => { return a.lastname.localeCompare(b.lastname)},
-    },    
+      sorter: (a, b) => { return a.lastname.localeCompare(b.lastname) },
+    },
     {
       title: 'Street address',
       dataIndex: 'streetaddress',
       key: 'streetaddress',
-      sorter: (a, b) => { return a.streetaddress.localeCompare(b.streetaddress)},
+      sorter: (a, b) => { return a.streetaddress.localeCompare(b.streetaddress) },
     },
     {
       title: 'Postcode',
@@ -122,18 +123,18 @@ export default function Customerlist()  {
       key: 'postcode',
       defaultSortOrder: 'descend',
       sorter: (a, b) => a.postcode - b.postcode,
-    },    
+    },
     {
       title: 'City',
       dataIndex: 'city',
       key: 'city',
-      sorter: (a, b) => { return a.city.localeCompare(b.city)},
+      sorter: (a, b) => { return a.city.localeCompare(b.city) },
     },
     {
       title: 'E-mail',
       dataIndex: 'email',
       key: 'email',
-      sorter: (a, b) => { return a.email.localeCompare(b.email)},
+      sorter: (a, b) => { return a.email.localeCompare(b.email) },
     },
     {
       title: 'Phone',
@@ -153,91 +154,300 @@ export default function Customerlist()  {
       render: (row) => (<Addtraining customer={row} >Add Training</Addtraining>)
     },
     */
-  ] 
+  ]
 
 
-
-
-  const [select, setSelect] = useState('');
+/*
+  const [select, setSelect] = useState('Select Column');
   const [value, setValue] = useState('');
-  const filterColumn = ['firstname', 'lastname','streetaddress', 'postcode', 'city', 'email', 'phone' ]
+  const filterColumn = ['firstName', 'lastname', 'streetaddress', 'postcode', 'city', 'email', 'phone']
   class Filter extends React.Component {
-    
     state = {
       column: filterColumn,
     };
-
     handleProvinceChange = value => {
       setSelect(value);
-      console.log(value)
-
       this.setState({
-        cities: filterColumn[value],
+        column: filterColumn[value],
       });
     };
-
     render() {
+
       const { column } = this.state;
       return (
         <>
-          <Select
-            defaultValue="Select Column"
-            style={{ width: 120 }}
-            onChange={this.handleProvinceChange}
-          >
-            {filterColumn.map(province => (
-              
-            <Option key={province}>{province}</Option>
-            ))}
-          </Select>        
+          <Input.Group>
+            <Select
+              value={select}
+              style={{ width: 120 }}
+              onChange={this.handleProvinceChange}
+            >
+              {filterColumn.map(column => (
+                <Option key={column}>{column}</Option>
+              ))}
+            </Select>
             {column.map(column => (
-              <Option key={column}></Option>         
+              <Option key={column} >{column}</Option>
             ))}
+            <Input
+              disabled={select == ''} // disable search when column not selected  
+              style={{ width: 250 }}
+              placeholder="Search"
+              value={value}
 
-      <Input
-      style={{width: 250}}
-      placeholder="Search"
-      value={value}
-      onChange={e => {
-        
-        
-        const currValue = e.target.value;
-        setValue(currValue);
-        console.log(customers)
-        const filteredData = customers.filter(entry =>
-          
-          entry[select].includes(currValue)               // <--- selected sisältää joko firstname, lastname, postcode jne...
-        );
-        setCustomers(filteredData);
-        if (customers.length == 0 || currValue == "") {
-          getCustomers();
-        }
-      }}>
-       
-      </Input>
+              onChange={e => {
+
+
+                const currValue = e.target.value;
+                setValue(currValue);
+                const filteredData = customers.filter(entry =>
+
+                  entry[select].includes(currValue)               // <--- selected sisältää joko firstname, lastname, postcode jne...
+                );
+                setCustomers(filteredData);
+                if (customers.length == 0 || currValue == "") {
+                  getCustomers();
+                }
+              }}>
+
+            </Input>
+          </Input.Group>
         </>
       );
-
-      
     }
   }
+*/
+
+
+class FilterTable extends React.Component {
+  state = {
+    searchText: '',
+    searchedColumn: '',
+  };
+
+  getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          icon={<SearchOutlined />}
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: text =>
+      this.state.searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#f2a130', padding: 0 }}
+          searchWords={[this.state.searchText]}
+          autoEscape
+          textToHighlight={text.toString()}
+          
+        />
+      ) : (
+        text
+      ),
+  });
+
+  handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    this.setState({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
+  };
+
+  handleReset = clearFilters => {
+    clearFilters();
+    this.setState({ searchText: '' });
+  };
+
+  render() {
+    const columns = [
+      {
+        title: 'First name',
+        
+        dataIndex: 'firstname',
+        key: 'firstname',
+        width: '30%',
+        ...this.getColumnSearchProps('firstname'),
+        sorter: (a, b) => { return a.firstname.localeCompare(b.firstname) }
+      },
+      {
+        title: 'Last name',
+        dataIndex: 'lastname',
+        key: 'lastname',
+        width: '20%',
+        ...this.getColumnSearchProps('lastname'),
+        sorter: (a, b) => { return a.lastname.localeCompare(b.lastname) }
+      },
+      {
+        title: 'Street address',
+        dataIndex: 'streetaddress',
+        key: 'streetaddress',
+        ...this.getColumnSearchProps('streetaddress'),
+        sorter: (a, b) => { return a.streetaddress.localeCompare(b.streetaddress) },
+      },
+      {
+        title: 'Postcode',
+        dataIndex: 'postcode',
+        key: 'postcode',
+        ...this.getColumnSearchProps('postcode'),
+        sorter: (a, b) => a.postcode - b.postcode,
+      },
+
+      {
+        title: 'City',
+        dataIndex: 'city',
+        key: 'city',
+        ...this.getColumnSearchProps('city'),
+        sorter: (a, b) => { return a.city.localeCompare(b.city) },
+      },
+
+      {
+        title: 'E-mail',
+        dataIndex: 'email',
+        key: 'email',
+        ...this.getColumnSearchProps('email'),
+        sorter: (a, b) => { return a.email.localeCompare(b.email) },
+      },
+
+      {
+        title: 'Phone',
+        dataIndex: 'phone',
+        key: 'phone',
+        ...this.getColumnSearchProps('phone'),
+        sorter: (a, b) => { return a.phone.localeCompare(b.phone) },
+      },
+    ];
+    return <Table columns={columns} dataSource={customers} bordered />;
+  }
+}
 
 
 
 
 
 
-  
-  return(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return (
 
     <div>
-      <Filter></Filter>
-      
-      
-      
-      <Table 
-        dataSource={customers} columns={columns} />
-      <Snackbar open={open} autoHideDuration={3000} 
+      <FilterTable></FilterTable>
+
+
+      <Snackbar open={open} autoHideDuration={3000}
         onClose={handleClose} message={msg} />
     </div>
   )
--- src/components/Traininglist.js
+++ src/components/Traininglist.js
@@ -1,19 +1,108 @@
 import React, { useState, useEffect } from 'react';
 import ReactTable from 'react-table-v6';
 import 'react-table-v6/react-table.css';
-import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
 import Edittraining from './Edittraining';
 import Addtraining from './Addtraining';
 import moment from 'moment';
+import { Table, Button, Input, Select } from 'antd';
 
-import { Table, Tag } from 'antd';
+import Highlighter from 'react-highlight-words'
+import { SearchOutlined } from '@ant-design/icons';
+
 import 'antd/dist/antd.css';
 
 export default function Traininglist() {
   const [trainings, setTrainings] = useState([]);
   const [open, setOpen] = useState(false);
   const [msg, setMsg] = useState('');
+
+
+
+
+
+
+
+
+
+
+
+  const { Option } = Select;
+  const [select, setSelect] = useState('');
+  const [value, setValue] = useState('');
+  const filterColumn = ['Activity', 'customer']
+
+  class Filter extends React.Component {
+    state = {
+      column: filterColumn,
+    };
+
+    handleProvinceChange = value => {
+      setSelect(value);
+      console.log(value)
+
+      this.setState({
+        column: filterColumn[value],
+      });
+    };
+
+    render() {
+      const { column } = this.state;
+      
+      return (
+        <>
+        <Input.Group>
+          <Select
+            defaultValue="Select Column"
+            style={{ width: 120 }}
+            onChange={this.handleProvinceChange}
+          >
+            
+            {filterColumn.map(province => (
+              
+            <Option key={province}>{province}</Option>
+            ))}
+          </Select>        
+            {column.map(column => (
+              <Option key={column}></Option>         
+            ))}
+      <Input
+      disabled={select == ''} // disable search when column not selected  
+      style={{width: 250}}
+      placeholder="Search"
+      value={value}
+      onChange={e => {
+        
+        
+        const currValue = e.target.value;
+        setValue(currValue);
+        const filteredData = trainings.filter(entry =>
+          entry[select.toLowerCase()].includes(currValue)               // <--- selected sisältää joko firstname, lastname, postcode jne...
+        );
+        setTrainings(filteredData);
+        if (trainings.length == 0 || currValue == "") {
+          getTrainings();
+        }
+      }}>
+      </Input>
+      </Input.Group>
+        </>
+      );
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   useEffect(() => {
@@ -86,6 +175,7 @@ export default function Traininglist() {
     setOpen(false);
   }
 
+/*
   const columns = [
     {
       title: 'Date',
@@ -121,13 +211,11 @@ export default function Traininglist() {
           <span> {customer.firstname} <span>
           </span> {customer.lastname} </span>
         </div>,
-       
-      
     }
     
 
     ,    
-       /*
+       
     {
       render: row => (<Edittraining training={row.original} updateTraining={updateTraining} />)
     }
@@ -140,17 +228,168 @@ export default function Traininglist() {
       minWidth: 60,
       Cell: row => (<Button color="secondary" size="small" onClick={() => deleteTraining(row.value)}>Delete</Button>)
     }  
-    */
   ]
+*/
+
+
+  class FilterTable extends React.Component {
+    state = {
+      searchText: '',
+      searchedColumn: '',
+    };
+  
+    getColumnSearchProps = dataIndex => ({
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div style={{ padding: 8 }}>
+          <Input
+            ref={node => {
+              this.searchInput = node;
+            }}
+            placeholder={`Search ${dataIndex}`}
+            value={selectedKeys[0]}
+            onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+            style={{ width: 188, marginBottom: 8, display: 'block' }}
+          />
+          <Button
+            type="primary"
+            onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90, marginRight: 8 }}
+          >
+            Search
+          </Button>
+          <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+        </div>
+      ),
+      filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+      onFilter: (value, record) =>
+        record[dataIndex]
+          .toString()
+          .toLowerCase()
+          .includes(value.toLowerCase()),
+      onFilterDropdownVisibleChange: visible => {
+        if (visible) {
+          setTimeout(() => this.searchInput.select());
+        }
+      },
+      render: text =>
+        this.state.searchedColumn === dataIndex ? (
+          <Highlighter
+            highlightStyle={{ backgroundColor: '#f2a130', padding: 0 }}
+            searchWords={[this.state.searchText]}
+            autoEscape
+            textToHighlight={text.toString()}
+            
+          />
+        ) : (
+          text
+        ),
+    });
+  
+    handleSearch = (selectedKeys, confirm, dataIndex) => {
+      confirm();
+      this.setState({
+        searchText: selectedKeys[0],
+        searchedColumn: dataIndex,
+      });
+    };
+  
+    handleReset = clearFilters => {
+      clearFilters();
+      this.setState({ searchText: '' });
+    };
+  
+    render() {
+      const columns = [
+        {
+          title: 'Date',
+          dataIndex: 'date',
+          key: 'date',
+          width: '30%',
+          ...this.getColumnSearchProps('date'),
+          sorter: (a, b) => { return a.date.localeCompare(b.date) },
+          render: (date) => 
+          <div>{moment(date).calendar()}</div>,
+        },
+        {
+          title: 'Duration',
+          dataIndex: 'duration',
+          key: 'duration',
+          width: '20%',
+          ...this.getColumnSearchProps('duration'),
+          sorter: (a, b) => { return a.duration.localeCompare(b.duration) }
+        },
+        {
+          title: 'Activity',
+          dataIndex: 'activity',
+          key: 'activity',
+          ...this.getColumnSearchProps('activity'),
+          sorter: (a, b) => { return a.activity.localeCompare(b.activity) },
+        },
+        {
+          title: 'Customer',
+          dataIndex: 'customer',
+          key: 'customer',
+          ...this.getColumnSearchProps('customer'),
+          sorter: (a, b) => {return a.customer.firstname.localeCompare(b.customer.firstname)},
+          render: (customer) => 
+          <div>
+            <span> {customer.firstname} <span>
+            </span> {customer.lastname} </span>
+          </div>,
+        }
+      ];
+      return <Table columns={columns} dataSource={trainings} bordered />;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   return(
     <div>
-      <Table 
-        dataSource={trainings} columns={columns} />
+      <FilterTable></FilterTable>
       <Snackbar open={open} autoHideDuration={3000} 
         onClose={handleClose} message={msg} />
     </div>
   )
 }
 
-/* <Addtraining addTraining={addTraining}/> */
+// <Addtraining addTraining={addTraining}/> */
